ac0ed82030dbd3eea8a6e65537b179b90aa0afb9
Hernán Arango Isaza <31634784+hernanarangoisaza@users.noreply.github.com>
2020-11-11 10:30:34 -0500

    ajustes multithreading

--- __pruebas.py
+++ __pruebas.py
@@ -1,9 +1,7 @@
-﻿
-
-    
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import PySimpleGUI as sg
-import time
 import threading
 import pandas
 
@@ -11,22 +9,22 @@ df_xlsx = None
 t1 = None
 t2 = None
 
-def long_function_thread(window):
-    df_xlsx = pandas.ExcelFile( values["-FILE-"] )
-    window.write_event_value('-THREAD DONE-', '')
-    print ( df_xlsx.sheet_names )
+def long_function_thread( window ):
+    global df_xlsx
+    df_xlsx = pandas.ExcelFile( values[ "-FILE-" ] )
+    window.write_event_value( '-THREAD DONE-', '' )
 
-def long_function_thread2(window):
+def long_function_thread2( window ):
     i = 0
-    window['-PROG-'].update_bar(0,0)
-    while (t1.is_alive()):
-        window['-PROG-'].update_bar(i, 100)
+    window[ '-PROG-' ].update_bar( 0, 0 )
+    while ( t1.is_alive() ):
+        window[ '-PROG-' ].update_bar( i, 100 )
         i = i + 1
         print ( i )
         if ( i == 99 ):
             i = 0
-            window['-PROG-'].update_bar(0,0)
-    window['-PROG-'].update_bar(100, 100)
+            window[ '-PROG-' ].update_bar( 0, 0 )
+    window[ '-PROG-' ].update_bar( 100, 100 )
 
 def long_function():
     global t1
@@ -41,16 +39,14 @@ def long_function2():
     t2.start()
 
 layout = [
-          [sg.Output(size=(60,10))],
-          [sg.Button('Go'), sg.Button('Nothing'), sg.Button('Exit')],
-          [sg.Text('Browse to a file')],
-          [sg.Input(key='-FILE-', visible=False, enable_events=True), sg.FileBrowse()],
-          [sg.Text('Work progress'), sg.ProgressBar(100, size=(20, 20), orientation='h', key='-PROG-')]
+          [ sg.Output( size=( 60, 10 ) ) ],
+          [ sg.Button( 'Go' ), sg.Button( 'Nothing' ), sg.Button( 'Exit' ) ],
+          [ sg.Text( 'Browse to a file' ) ],
+          [ sg.Input( key='-FILE-', visible=False, enable_events=True ), sg.FileBrowse() ],
+          [ sg.Text( 'Work progress' ), sg.ProgressBar( 100, size=( 20, 20 ), orientation='h', key='-PROG-' ) ]
          ]
 
-window = sg.Window('Window Title', layout)
-
-
+window = sg.Window( 'Window Title', layout )
 
 while True:             # Event Loop
     event, values = window.read()
@@ -62,9 +58,12 @@ while True:             # Event Loop
         long_function()
         long_function2()
         print('Long function has returned from starting')
+
     elif event == '-THREAD DONE-':
         print('Your long operation completed')
-        #print( respuesta.sheet_names )
+        print ( df_xlsx.sheet_names )
+
     else:
         print(event, values)
+
 window.close()
