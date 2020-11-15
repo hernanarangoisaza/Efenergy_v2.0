39caeca2dd2c99ad93d2e3731cdd5a1ce1b46a93
Hernán Arango Isaza <31634784+hernanarangoisaza@users.noreply.github.com>
2020-11-15 08:36:06 -0500

    ajustes

--- __pruebas.py
+++ __pruebas.py
@@ -20,7 +20,7 @@ SYMBOL_DOWN = '▼'
 rutaLogoPrincipal = 'imagenes/logo_texto_2020.png'
 rutaIconoPrincipal = 'imagenes/logo_2020.ico'
 
-extensionesPlantillas = (("Excel", "*.xls*"),) 
+extensionesPlantillas = (("Excel", "*.xls*"),)
 barraEstado = 'SENA - CDITI - TEINNOVA - Semillero de Energías. Todos los derechos reservados. (C) 2020'
 
 eColor1 = 'black'
@@ -34,11 +34,11 @@ archivoPlantilla = None
 
 # BARRA DE MENÚ PRINCIPAL TODO ACTIVO
 menuPrincipal1 =     [
-                        [ 'Opciones', [ 'Acerca de...::-OPC ACERCADE-', '---', 'Salir' ] ],                    
-                        [ 'Voltaje', 
+                        [ 'Opciones', [ 'Acerca de...::-OPC ACERCADE-', '---', 'Salir' ] ],
+                        [ 'Voltaje',
                             [ 'Analizar Voltaje::-OPC V1-' ]
                         ],
-                        [ 'Potencia', 
+                        [ 'Potencia',
                             ['Analizar Factor de Potencia::-OPC P1-', 'Potencia Reactiva::-OPC P2-' ]
                         ],
                         [ 'Armónicos',
@@ -53,11 +53,11 @@ menuPrincipal1 =     [
 
 # BARRA DE MENÚ PRINCIPAL ITEMS BLOQUEADOS
 menuPrincipal2 =     [
-                        [ 'Opciones', [ 'Acerca de...::-OPC ACERCADE-', '---', 'Salir' ] ],                    
-                        [ '!Voltaje', 
+                        [ 'Opciones', [ 'Acerca de...::-OPC ACERCADE-', '---', 'Salir' ] ],
+                        [ '!Voltaje',
                             [ 'Analizar Voltaje::-OPC V1-' ]
                         ],
-                        [ '!Potencia', 
+                        [ '!Potencia',
                             ['Analizar Factor de Potencia::-OPC P1-', 'Potencia Reactiva::-OPC P2-' ]
                         ],
                         [ '!Armónicos',
@@ -71,49 +71,49 @@ menuPrincipal2 =     [
                     ]
 
 # SELECTOR DE PLANTILLAS DE ORIGEN DE DATOS
-frameLayout1 =  [ 
-                    [ 
+frameLayout1 =  [
+                    [
                         sg.Input(key='-SELECCION PLANTILLA-', visible=True, enable_events=True, size=(85,1), font=("Consolas",9), readonly=True, pad=((10,0),(5,5))),
                         sg.FileBrowse(button_text='Explorar', key='-BTN PLANTILLA-', size=(10,1), button_color=eColores1, file_types=extensionesPlantillas)
                     ],
-                    [ 
+                    [
                         sg.Text('Ruta:', key='-LRUTA PLANTILLA-', size=(6,1), text_color=eColor1, background_color=eColor2),
-                        sg.Text('---', key='-VRUTA PLANTILLA-' , size=(80,1), text_color=eColor1, background_color=eColor2, font=("Consolas",9)+('bold',)) 
+                        sg.Text('---', key='-VRUTA PLANTILLA-' , size=(80,1), text_color=eColor1, background_color=eColor2, font=("Consolas",9)+('bold',))
                     ],
-                    [ 
-                        sg.Text('Archivo:', key='-LARCHIVO PLANTILLA-', size=(6,1), text_color=eColor1, background_color=eColor2), 
-                        sg.Text('---', key='-VARCHIVO PLANTILLA-' , size=(80,1), text_color=eColor1, background_color=eColor2, font=("Consolas",9)+('bold',)) 
+                    [
+                        sg.Text('Archivo:', key='-LARCHIVO PLANTILLA-', size=(6,1), text_color=eColor1, background_color=eColor2),
+                        sg.Text('---', key='-VARCHIVO PLANTILLA-' , size=(80,1), text_color=eColor1, background_color=eColor2, font=("Consolas",9)+('bold',))
                     ]
                 ]
 
 barraMenuPrincipal = sg.Menu(menuPrincipal1, key='-MENU PRINCIPAL-')
 frameSelectorPlantilla = sg.Frame('  Seleccionar plantilla de origen de datos  ', frameLayout1, pad=((15,15),(0,0)), title_color=eColor1, background_color=eColor2)
-logoPrincipal = sg.Image(filename=rutaLogoPrincipal, background_color=eColor2, size=(600,100))
+logoPrincipal = sg.Image(filename=rutaLogoPrincipal, background_color=eColor2, size=(600,100), key='-LOGO PRINCIPAL-')
 statusBarPrincipal = sg.StatusBar(text=barraEstado, size=(1200,1), pad=((50,50),(20,20)), text_color=eColor1, background_color=eColor2, relief=sg.RELIEF_FLAT, justification='center', visible=True, key='status_bar')
 
 # FULL LAYOUT
 layout =    [
                 #### Barra de Menú superior principal
-                [ 
+                [
                     barraMenuPrincipal
                 ],
                 #### Logo
-                [ 
-                    logoPrincipal  
+                [
+                    logoPrincipal
                 ],
                 #### Barra de estado
-                [ 
+                [
                     statusBarPrincipal
                 ],
                 #### Selector de plantilla
-                [ 
+                [
                     frameSelectorPlantilla
                 ],
             ]
 
 # ************************************************************************************************************************
 
-# Cambiar al tema personalizado 
+# Cambiar al tema personalizado
 sg.theme('Default1')
 sg.ChangeLookAndFeel('SystemDefault')
 
@@ -136,8 +136,11 @@ window = sg.Window(
 
 barraMenuPrincipal.Update(menuPrincipal2)
 frameSelectorPlantilla.Update(visible=True)
-logoPrincipal.Update(visible=False)
+logoPrincipal.Update(visible=True)
 statusBarPrincipal.Update(visible=False)
+barraMenuPrincipal.Update(visible=False)
+window.refresh()
+
 
 # ************************************************************************************************************************
 
@@ -147,17 +150,17 @@ while True:
     event, values = window.read()
 
     print(event)
-    
+
     # Salir de la aplicación
     if event == sg.WIN_CLOSED or event == 'Salir':
 
-        break 
-    
+        break
+
     # Salir de la aplicación
     if event.endswith('-OPC SALIR-'):
 
         break
-    
+
     if event == '-SELECCION PLANTILLA-':
 
         rutaPlantilla = values['-SELECCION PLANTILLA-']
@@ -177,7 +180,12 @@ window.close()
 # size(width,height)
 # pad=((left,right), (top,bottom))
 # if event.startswith('-XXXX-'):
-# if event.endswith('-XXXX-'): --> Necesario para identicar los key en las opciones de menús --> (option::-KEY-) 
+# if event.endswith('-XXXX-'): --> Necesario para identicar los key en las opciones de menús --> (option::-KEY-)
 # menuPrincipal[1][0] = '!' + menuPrincipal[1][0]
+# window['-LOGO PRINCIPAL-'].hide_row()
+# window['-LOGO PRINCIPAL-'].unhide_row()
+# bkSizeLogo = logoPrincipal.get_size()
+# logoPrincipal.set_size((1,None))
+# window.refresh()
 
 # ************************************************************************************************************************
--- _pySGdemos.py
+++ _pySGdemos.py
@@ -1,59 +1,45 @@
-"""  
-Menu Update
-
-How to change menu1 to menu2 and vice versa without creating a second window?
-
-Intially menu1 is loaded, its mode is Neutral, to change to Play mode, we
-press Mode->Play which would bring us to menu2. Press Mode->Neutral to bring
-us to initial Neutral mode again.
-
-"""  
-
 import PySimpleGUI as sg
 
-# Constants
-menu1 = [
-        ['File', ['Quit']],
-        ['Mode', ['Play']],
-        ['Engine', ['Setting']]
-]
+"""
+    Demo - Multiple layouts in a single window that are swapped in and out
 
-menu2 = [
-        ['File', ['Quit']],
-        ['Mode', ['Neutral']],
-        ['Help', ['How to Play']]
-]
+    If you've ever wanted to replace the contents of a window with another layout then perhaps
+    this demo is for you.  You cannot actually change the layout of a window dynamically, but what
+    you can do is make elements visible and invisible.  
+    
+    To "swap out" a portion of a window, use a Column element for that portion.  Add multiple Columns
+    on the same row and make only 1 of them active at a time
+"""
 
-# Create variables for our elements. We will use this for updates.
-menu_elem = sg.Menu(menu1)
-txt_elem = sg.Text('Layout, mode = Neutral')
+# ----------- Create the 3 layouts this Window will display -----------
+layout1 = [[sg.Text('This is layout 1 - It is all Checkboxes')],
+           *[[sg.CB(f'Checkbox {i}')] for i in range(5)]]
 
-# Create layout for our elements
-layout = [
-        [menu_elem],
-        [txt_elem],
-]
+layout2 = [[sg.Text('This is layout 2')],
+           [sg.Input(key='-IN-')],
+           [sg.Input(key='-IN2-')]]
 
-w = sg.Window('Menu Update', layout)
+layout3 = [[sg.Text('This is layout 3 - It is all Radio Buttons')],
+           *[[sg.R(f'Radio {i}', 1)] for i in range(8)]]
 
+# ----------- Create actual layout using Columns and a row of Buttons
+layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
+          [sg.Button('Cycle Layout'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
+
+window = sg.Window('Swapping the contents of a window', layout)
+
+layout = 1  # The currently visible layout
 while True:
-    e, v = w.Read(timeout=100)
-    
-    # If Quit menu entry is pressed or X is pressed
-    if e == 'Quit' or e is None:
+    event, values = window.read()
+    print(event, values)
+    if event in (None, 'Exit'):
         break
-    
-    # Change to Play mode, load menu2
-    if e == 'Play':
-        print('Hits Play')
-        menu_elem.Update(menu2)
-        txt_elem.Update('Layout, mode = Play')
-        continue
-        
-    # Change to Neutral mode, load menu1
-    if e == 'Neutral':
-        print('Hits Neutral')
-        menu_elem.Update(menu1)
-        txt_elem.Update('Layout, mode = Neutral')
-
-w.Close()
+    if event == 'Cycle Layout':
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = layout + 1 if layout < 3 else 1
+        window[f'-COL{layout}-'].update(visible=True)
+    elif event in '123':
+        window[f'-COL{layout}-'].update(visible=False)
+        layout = int(event)
+        window[f'-COL{layout}-'].update(visible=True)
+window.close()
