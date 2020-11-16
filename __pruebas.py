5e1ae59c17f35b16f07179ee8423b3d951e6b649
Hernán Arango Isaza <31634784+hernanarangoisaza@users.noreply.github.com>
2020-11-15 19:53:32 -0500

    ajustes

--- __pruebas.py
+++ __pruebas.py
@@ -17,15 +17,26 @@ MENU_KEY_SEPARATOR = '::'
 SYMBOL_UP = '▲'
 SYMBOL_DOWN = '▼'
 
+sizeFrmPrincipal = (1030,600)
+sizeColumnas = (1000,580)
+sizeSelectorPlantilla = (1000,580)
+
+filtroVoltaje = ['MENOR','RANGO','MAYOR']
+filtroFases = ['A','B','C']
+
+fontRutaPartes = ("Consolas",10)+('bold',)
+fontRutaTotal = ("Consolas",10)
+
 rutaLogoPrincipal = 'imagenes/logo_texto_2020.png'
 rutaIconoPrincipal = 'imagenes/logo_2020.ico'
 
-extensionesPlantillas = (("Excel", "*.xls*"),)
+extensionesPlantillas = (("Excel","*.xls*"),)
 barraEstado = 'SENA - CDITI - TEINNOVA - Semillero de Energías. Todos los derechos reservados. (C) 2020'
 
 eColor1 = 'black'
 eColor2 = '#F2F2F2'
-eColores1 = ('black', '#CDCDCD')
+eColor3 = 'white'
+eColores1 = ('black','#CDCDCD')
 
 rutaPlantilla = None
 archivoPlantilla = None
@@ -73,23 +84,200 @@ menuPrincipal2 =     [
 # SELECTOR DE PLANTILLAS DE ORIGEN DE DATOS
 frameLayout1 =  [
                     [
-                        sg.Input(key='-SELECCION PLANTILLA-', visible=True, enable_events=True, size=(85,1), font=("Consolas",9), readonly=True, pad=((10,0),(5,5))),
-                        sg.FileBrowse(button_text='Explorar', key='-BTN PLANTILLA-', size=(10,1), button_color=eColores1, file_types=extensionesPlantillas)
+                        sg.Input(key='-SELECCION PLANTILLA-', 
+                                 visible=True, 
+                                 enable_events=True, 
+                                 size=(121,1), 
+                                 font=fontRutaTotal, 
+                                 readonly=True, 
+                                 pad=((10,0),(5,5))),
+                        sg.FileBrowse(key='-BTN PLANTILLA-', 
+                                      button_text='Explorar', 
+                                      size=(10,1), 
+                                      button_color=eColores1, 
+                                      file_types=extensionesPlantillas, 
+                                      pad=((10,10),(10,10)))
                     ],
                     [
-                        sg.Text('Ruta:', key='-LRUTA PLANTILLA-', size=(6,1), text_color=eColor1, background_color=eColor2),
-                        sg.Text('---', key='-VRUTA PLANTILLA-' , size=(80,1), text_color=eColor1, background_color=eColor2, font=("Consolas",9)+('bold',))
+                        sg.Text(key='-LRUTA PLANTILLA-', 
+                                text='Ruta:', 
+                                size=(6,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                pad=((10,0),(0,10))),
+                        sg.Text(key='-VRUTA PLANTILLA-', 
+                                text='---', 
+                                size=(80,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                font=fontRutaPartes, 
+                                pad=((10,0),(0,10)))
                     ],
                     [
-                        sg.Text('Archivo:', key='-LARCHIVO PLANTILLA-', size=(6,1), text_color=eColor1, background_color=eColor2),
-                        sg.Text('---', key='-VARCHIVO PLANTILLA-' , size=(80,1), text_color=eColor1, background_color=eColor2, font=("Consolas",9)+('bold',))
+                        sg.Text(key='-LARCHIVO PLANTILLA-', 
+                                text='Archivo:', 
+                                size=(6,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                pad=((10,0),(0,10))),
+                        sg.Text(key='-VARCHIVO PLANTILLA-', 
+                                text='---', 
+                                size=(80,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                font=fontRutaPartes, 
+                                pad=((10,0),(0,10)))
                     ]
                 ]
 
-barraMenuPrincipal = sg.Menu(menuPrincipal1, key='-MENU PRINCIPAL-')
-frameSelectorPlantilla = sg.Frame('  Seleccionar plantilla de origen de datos  ', frameLayout1, pad=((15,15),(0,0)), title_color=eColor1, background_color=eColor2)
-logoPrincipal = sg.Image(filename=rutaLogoPrincipal, background_color=eColor2, size=(600,100), key='-LOGO PRINCIPAL-')
-statusBarPrincipal = sg.StatusBar(text=barraEstado, size=(1200,1), pad=((50,50),(20,20)), text_color=eColor1, background_color=eColor2, relief=sg.RELIEF_FLAT, justification='center', visible=True, key='status_bar')
+logoPrincipal = sg.Image(key='-LOGO PRINCIPAL-', 
+                         filename=rutaLogoPrincipal, 
+                         background_color=eColor2, 
+                         size=(965,100))
+
+statusBarPrincipal = sg.StatusBar(key='-STATUS BAR-', 
+                                  text=barraEstado, 
+                                  size=(1,1), 
+                                  pad=((0,0),(20,20)), 
+                                  text_color=eColor1, 
+                                  background_color=eColor2, 
+                                  relief=sg.RELIEF_FLAT, 
+                                  justification='center', 
+                                  visible=True)
+
+# FRAME LOGO
+layoutLogo =    [
+                    #### Logo
+                    [
+                        logoPrincipal
+                    ],
+                    #### Barra de estado
+                    [
+                        statusBarPrincipal
+                    ],
+                ]
+
+frameLogo = sg.Frame(key='-FRAME LOGO', 
+                     title='', 
+                     layout=layoutLogo, 
+                     title_color=eColor1, 
+                     background_color=eColor2)
+
+barraMenuPrincipal = sg.Menu(key='-MENU PRINCIPAL-', menu_definition=menuPrincipal1)
+
+frameSelectorPlantilla = sg.Frame(key='-FRAME SELECTORPLANTILLA', 
+                                  title='  Seleccionar plantilla de origen de datos  ', 
+                                  layout=frameLayout1, 
+                                  title_color=eColor1,
+                                  background_color=eColor2)
+
+comboboxDias = sg.Combo(key='-COMBOBOX DIAS', 
+                        values=[], 
+                        size=(10,1),
+                        auto_size_text=False,
+                        background_color=eColor3,
+                        text_color=eColor1)
+
+comboboxVoltaje = sg.Combo(key='-COMBOBOX VOLTAJE', 
+                         values=filtroVoltaje, 
+                         size=(10,1),
+                         auto_size_text=False,
+                         background_color=eColor3,
+                         text_color=eColor1)
+
+comboboxFases = sg.Combo(key='-COMBOBOX FASES', 
+                         values=filtroFases, 
+                         size=(10,1),
+                         auto_size_text=False,
+                         background_color=eColor3,
+                         text_color=eColor1)
+
+# FRAME FILTROS
+inputVariacion = sg.Input(key='-VARIACION-', 
+                          visible=True, 
+                          enable_events=True, 
+                          size=(4,1), 
+                          pad=((10,0),(2,0)), 
+                          text_color=eColor1, 
+                          background_color=eColor3, 
+                          justification='center')
+layoutFiltros =    [
+                    [
+                        #### Días disponibles
+                        sg.Text(key='-LCOMBOBOX DIAS-', 
+                                text='Días:', 
+                                size=(6,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                pad=((10,0),(2,10))),
+                        comboboxDias,
+                        #### Voltaje RANGO, MENOR, MAYOR
+                        sg.Text(key='-LCOMBOBOX VOLTAJE-', 
+                                text='Voltaje:', 
+                                size=(6,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                pad=((10,0),(2,10))),
+                        comboboxVoltaje,
+                        #### Fase A, B, C
+                        sg.Text(key='-LCOMBOBOX FASES-', 
+                                text='Fases:', 
+                                size=(6,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                pad=((10,0),(2,10))),
+                        comboboxFases,
+                        #### Límite variaciones redes eléctricas -10% 120 +10%
+                        sg.Text(key='-L1 VARIACION-', 
+                                text='Límites:', 
+                                size=(6,1), 
+                                text_color=eColor1,
+                                background_color=eColor2, 
+                                pad=((10,0),(2,10))),
+                        sg.Text(key='-L2 VARIACION-', 
+                                text='-10%', 
+                                size=(5,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                pad=((10,0),(2,10))),
+                        inputVariacion,
+                        sg.Text(key='-L3 VARIACION-', 
+                                text='+10%', 
+                                size=(5,1), 
+                                text_color=eColor1, 
+                                background_color=eColor2, 
+                                pad=((10,20),(2,10))),
+                    ],
+                ]
+
+inputVariacion.Update = 120
+
+frameFiltros = sg.Frame(key='-FRAME FILTROS', 
+                        title='  Filtros  ', 
+                        layout=layoutFiltros, 
+                        title_color=eColor1, 
+                        background_color=eColor2)
+
+layoutColumna1 =    [
+                        #### Logo + Barra
+                        [
+                            frameLogo
+                        ],
+                        #### Selector de plantilla
+                        [
+                            frameSelectorPlantilla
+                        ],
+                        #### Sección de filtros
+                        [
+                            frameFiltros
+                        ],
+                    ]
+
+columna1 = sg.Column(key='-COLUMNA 1', 
+                     layout=layoutColumna1, 
+                     visible=True, 
+                     background_color=eColor2, 
+                     size=sizeColumnas)
 
 # FULL LAYOUT
 layout =    [
@@ -97,17 +285,10 @@ layout =    [
                 [
                     barraMenuPrincipal
                 ],
-                #### Logo
+                #### Columnas ocultables para simular pantallas
                 [
-                    logoPrincipal
-                ],
-                #### Barra de estado
-                [
-                    statusBarPrincipal
-                ],
-                #### Selector de plantilla
-                [
-                    frameSelectorPlantilla
+                    columna1, 
+                    #columna2
                 ],
             ]
 
@@ -123,9 +304,7 @@ window = sg.Window(
         'Efenergy v2.0',
         layout,
         use_default_focus=True,
-        size=(750,600),
-        #element_padding=(0, 0),
-        #margins=(10, 10),
+        size=sizeFrmPrincipal,
         debugger_enabled=False,
         finalize=True,
         font=("Helvetica",11),
@@ -134,13 +313,10 @@ window = sg.Window(
 
 # ************************************************************************************************************************
 
+# Habilitar barra de menú con opciones deshabilitadas
 barraMenuPrincipal.Update(menuPrincipal2)
-frameSelectorPlantilla.Update(visible=True)
-logoPrincipal.Update(visible=True)
-statusBarPrincipal.Update(visible=False)
-barraMenuPrincipal.Update(visible=False)
-window.refresh()
 
+window.refresh()
 
 # ************************************************************************************************************************
 
@@ -170,7 +346,14 @@ while True:
         barraMenuPrincipal.Update(menuPrincipal1)
 
     # Analizar Voltaje
-    #if event.endswith('-OPC N1-'):
+    if event.endswith('-OPC V1-'):
+        rutaPlantillaVoltaje = values['-SELECCION PLANTILLA-']
+        archivo_voltaje = pandas.ExcelFile(rutaPlantillaVoltaje)
+        comboboxDias.Update(values=archivo_voltaje.sheet_names)
+      
+
+        
+        
 
 window.close()
 
@@ -187,5 +370,6 @@ window.close()
 # bkSizeLogo = logoPrincipal.get_size()
 # logoPrincipal.set_size((1,None))
 # window.refresh()
+# window['-COLUMNA 2'].Update(visible=False)
 
 # ************************************************************************************************************************
--- _pySGdemos.py
+++ _pySGdemos.py
@@ -1,45 +1,24 @@
 import PySimpleGUI as sg
+import os
+import time
+import random
 
-"""
-    Demo - Multiple layouts in a single window that are swapped in and out
+exit_button = b'iVBORw0KGgoAAAANSUhEUgAAADAAAAAwCAYAAABXAvmHAAAJ7ElEQVR42s2ZCXAT1xnHv11J60O2aUkpd0IKZYA2AVKH0AAuIZSSEI4kDiRu2qa0dDKdMG1JmpgrhFDCUSiB4T7CaYi5bMBctjHY+MTFGHzIyPIB8n1hW7Kk1UpWv/dWK8sWNna0nnRn3uzbXel7/9/3vu97byUGvDzSvv6tw1sbPTlYBQsvLT7KSNeMN8a+DwBy/PLvEfIBpPwnjAKk5pfD1dulsolkGAYGDR8NA0aMhXqLP1SXVkH4lEb6bNKS4/IB3Nz0HgWITCyA7RfueCV45JjnYPzESeD7o2dA8OkHhVVK0FRyoLK1QoBRCxFztfSzUz45IR9A0sZ3RYCkAtgRk92j7w4fNQZefDkEBqCnrf79oKCBgczaQOB5BXA2G4BZAM5up30/gxYOv66h3wv59Fv5AG6sX0ABTt68Dzsvdg0wbMRICEbBo8YFg+MH/aHQyECaIQj01iAAixUFW1EsChZs9JoT7CIANkWDBg7OzKd2poZHygeQ8NV8CnAq+T7sunS33bOhw34CwZNCYPxLk8B/4FAoMjkgjVdDDvc02Hk7FSwKF0TBVpvoeYtAzxQGxZNrn2Yt7Jl2j9qdtuykfADx/wqlAKdTtLD78j3XfU4dAP88Eg0Zgi9k9B0JBsbPJVQULrQDIF7nBEEUT/o0dFA8L2AO2KDl4V049kYBtT19xWn5AOK+fFsESC2EPVfaAHzUgfB5QR0sr+Y8PN0RgLPaneLbQkjyPDm3NJrBWJoFZ0OLqO1ff35GPoCrq96iAGfSCmFvbE6PAYhwAiDGvDNsSBjxgmsGiosbQN2sgdNv6qjt36w+Kx/A5ZVvUoCz6YWwLy63RwCcVRA9bybCBReA5HkV9vVljWBsMEGgoQBOzi2ktl9bEyUfwMXl8yhAVIYO9sd3H4CKRe8C7wwhkrRCm+c5uw2aGsxQXt5EPx9o0MC3s0WAWWuj5QOIWTpXBLilgwPX8roFQIRTsRKI4FZ9nHW/Ffv3ix6Bw8y7AE7MEheyN9ad8x6gYN8+x6hFi5jzn82mANGZRfBNQv4TAWjYWAW3EHLzvDN5lVj/dQ8fganR4poxAhDxmliF5my44D1A8rx5Dl3MeX3Qn18Zygb6wbnMYjh4o2sAjkeBVin+xbIpLlptdZ/EfU2tESqrDO1CLgCT+NhMcSWe9+8Y7wFOsqzdd8RwvbFC/4zqnYlwsbgaDiVqOgWg3ualBUsQV1kyE7yzjwCk3luMVtA+aHSGTnuAIzNEB721+aL3APsVCvvM5cvZnMvnwJirgcTRg2F7VsljAWjM826Vx9rB84Kdinfgta6sGUxNZo+kJwCHp4s59vaWS94DfIEz8AECNKengy7/DjiqGmCFvRWkOZAAVtcigMVtqyDlgBTzxPOCWO/r6k1QXm18bNUiAAdfEavcO9suew+wimXL31+yZFDZjh3QiiLKfJQQZDTDanyW7Qawro5zJq8zB6TtAvG8VQwbwPs2FJn/oAlDx9opwIFfiQvlgu1XPAF+xzAOM+k4HLwvnnDnAgFuLdCtr8ZPZXAcN2vCBLY5M9MFlYPi/FpbYSf2890AaAhJOSD1ScI6qw+pOho9hk6z2WOP5AJo0sC+EHGz+N7O2McAoKkVoaEc6dscDgwGPKPXGJUKGI5jWF9fUPn5AcuyoPD3B4fdztQePco4yJ7F7cgymcBssUAUAsxCgM31bQCujRqCSmFDZqKyzgxlNcbHbvLaAPJh72QRIGx3nCfAZ+jVhaGhtG9XKMDGYxVA8QoUrlSraWOc4kkzJCeDKdtz/9+KQDkIoUGBP0SAbQ2cm+cl8WLYEBgexd170NzpLlW6r0aAPS+L472/N94TYCuafNU5A3YSpyhehaJVAQGiaKVSPOMsAD6vO3SIhBt9FZQOB147MIRIy7U74GFyAex6xDk9LwKo3BYtJbZsvQHMBr5bALsmiq+sv99/zRPgFOoeHRrK2knY+PhQ4aQRz7MoXonCFdgnM9KSlAR8Xh6dkccBtGITOF+IiU6mAFLlUZFdp3MDR3KgpMYEFVh5unpPcAfYOSGLjvOHbxI8AWIQYOicOSwRwQUGiuIJBAkdDCkCQrzPYIgYIyPFL3cGgHG9r6QUArP0cKBJDKFnS7Lhx+WFkPuzmdT7zbhg5ZQbu3xP6AiwPfg2HeePh657ApwAaBo9e3YQFe8EII2IVznFsxhCfEoK2HU6Oiuo3gOA5MDu1FSwsUpQYw5ENHOgwnjfqkyHEFUdzKiaDEZzK2RXtOCqy/cIYNsLYsX705FET4A9CDAlLCzIXTw5uKAgMe6JQJx6W26uC8CHzAjOgnTwGH6HExJoFdqKVehjBDhpIDlgg4FVRdC/rgx0Q16AknozVDRYnvim1g6gMR++HneLjrMoIskT4AOAOq1CEYQeVGLNt2FTdVzl8J6AWywlbj+ZFpYtilqwYLg0AyYrDyfOX4B6hFhPqpFzHSAAUvUhlcdoEuBuZUunQrsC2PJ8Bh3rLydudroSK6Hzw+Z+sYBlyyIWLhxMAAwWMxyLOgN6XIm34DMc0rUSRxtFAJK8LCZxZpXJGTo9B9j883Q69oeRyd5vJT5EgF2LFw+uMTTBodOnoMlhhw0GC0jLmgRwqYUTaz4mtraeh/JGS5dCO7vvjwCbxqRR2389leI9QDgCfLRwYcCO48f6QF81ZKl94Or9CtdzdwBSdZowdLKqLU8U2hXAxlGp1PZHZ1Jl2MwxTHE+y/QdMOSpPv1GDYE75Q0QlfvAAyDehDOAIZRawwPfYvUKYMPIZGp7cVS69wBhAGk1at/SkEmj3+3jx8F1XSVE5z30AEiycJDTIEBFM98toZ0D5MG6ESLA385leA/wKtq5hjuQLbMn0HfiG0WVcC5f3w5gFQKcwSS+Xdd9oZ0CPMqDtcNvUtv/uHBLll8lfLDxm19/kQIkllTBeU17AOmFxtoiD8CaYUnU9seXMuX7WWXTzGAXwIX7ZR4A3flpsbsAXz6dSG1/cuW/8gFsnPELCpBUWg0x2t4F+GLIDWr709jb8gGsnz6eAtx8UAMXC8t7DcAPAVYNSqC2w+PvyAfw1bRxFCD5YQ1c0nmuA7IBNOTByoHXqO1lCdnyAaydOlYE0NfC5aLeBVjRP57aXn7jrnwAa0KepwApZbVwpbiyVwGW9Yujtlcm3ZMPYPXk58S/Wctr4SpWot4ECH8qltpelZwjH0BXx7oKu7C0At9sviuAySooTY13fQ2VmQNaNNd1mshTHcfoVYD1VQ5teBn8tIcA+XiOxes4vJ8IMfNbuhrj/wGgHu/HiYKxbZuq78kY3weAgNdpCEAEX8HrLAgf2/pdx+jdHKhy5C0tgzEoVItCY52evg7zBxjkGqNXAcJKHM8er8dOMFPitbFOjv8BWgbOqQUuR6kAAAAASUVORK5CYII='
 
-    If you've ever wanted to replace the contents of a window with another layout then perhaps
-    this demo is for you.  You cannot actually change the layout of a window dynamically, but what
-    you can do is make elements visible and invisible.  
-    
-    To "swap out" a portion of a window, use a Column element for that portion.  Add multiple Columns
-    on the same row and make only 1 of them active at a time
-"""
+layout = [  [sg.Text('My Window')],
+            [sg.Button('Go'),
+             sg.Button(image_data=exit_button, border_width=0,
+                       button_color=(sg.theme_background_color(), sg.theme_background_color()),
+                       key='Exit')]]
 
-# ----------- Create the 3 layouts this Window will display -----------
-layout1 = [[sg.Text('This is layout 1 - It is all Checkboxes')],
-           *[[sg.CB(f'Checkbox {i}')] for i in range(5)]]
+window = sg.Window('Window Title', layout)
 
-layout2 = [[sg.Text('This is layout 2')],
-           [sg.Input(key='-IN-')],
-           [sg.Input(key='-IN2-')]]
-
-layout3 = [[sg.Text('This is layout 3 - It is all Radio Buttons')],
-           *[[sg.R(f'Radio {i}', 1)] for i in range(8)]]
-
-# ----------- Create actual layout using Columns and a row of Buttons
-layout = [[sg.Column(layout1, key='-COL1-'), sg.Column(layout2, visible=False, key='-COL2-'), sg.Column(layout3, visible=False, key='-COL3-')],
-          [sg.Button('Cycle Layout'), sg.Button('1'), sg.Button('2'), sg.Button('3'), sg.Button('Exit')]]
-
-window = sg.Window('Swapping the contents of a window', layout)
-
-layout = 1  # The currently visible layout
-while True:
+while True:             # Event Loop
     event, values = window.read()
     print(event, values)
     if event in (None, 'Exit'):
         break
-    if event == 'Cycle Layout':
-        window[f'-COL{layout}-'].update(visible=False)
-        layout = layout + 1 if layout < 3 else 1
-        window[f'-COL{layout}-'].update(visible=True)
-    elif event in '123':
-        window[f'-COL{layout}-'].update(visible=False)
-        layout = int(event)
-        window[f'-COL{layout}-'].update(visible=True)
 window.close()
+
+# no titlebar, grab anywhere, alpht .8, black
+exit()
